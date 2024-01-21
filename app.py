afa2dda081d6d3d6c444295f429d760c241f26c3
Tokilaka <91784722+Tokilaka@users.noreply.github.com>
2024-01-21 19:10:10 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
 if __name__ == '__main__':
   from routes import *
   app.run() 
-  
-  
-  
-# flash message in flask --Flash-- google it for more info
-# also dont forget about redirect function in flask it is important
